07c950c6e83e09af5e9853de4cf9ae192ed01d46
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-16 17:15:45 -0500

    Add Login component

---
+++ src/app/Login/index.js
@@ -0,0 +1,11 @@
+const Login = () => (
+  <section>
+    <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </form>
+  </section>
+);
+
+export default Login;
