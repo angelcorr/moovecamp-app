4a7e431c170455a448a3a25ecced65d184f78ea7
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 12:12:49 -0500

    Add Sign Up component

---
+++ src/app/SignUp/index.js
@@ -0,0 +1,11 @@
+const SignUp = () => (
+  <form>
+    <input placeholder="Email" />
+    <input placeholder="Confirm your email" />
+    <input placeholder="Password" />
+    <input placeholder="Confirm your Password" />
+    <button>Sign Up</button>
+  </form>
+);
+
+export default SignUp;
