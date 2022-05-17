23361b2eba855df47811bc02f3aef90fbf7772f3
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 16:16:26 -0500

    Improve style in Sticky component

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,5 +1,5 @@
 const Sticky = () => (
-  <div className="w-96 bg-yellow-300 border border-yellow-300 rounded-md">
+  <div className="w-96 m-1 bg-yellow-300 border border-yellow-300 rounded-md">
     <div className="flex justify-between p-2 border border-yellow-700 rounded-md">
       <p className="w-auto">Title</p>
       <div className="w-10 flex justify-between">
