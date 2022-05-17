53cfacd44b135766b6e0ac711eb175cc9289f632
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 12:14:44 -0500

    Add Login component and finish first prototype

--- src/app/Login/index.js
+++ src/app/Login/index.js
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Login = () => (
-  <section>
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
-    </form>
+  <section className="bg-sticky-background-login bg-cover bg-center">
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-96 h-96 flex flex-col justify-evenly items-center border border-purple-800 rounded-md">
+        <p className="text-2xl">Log In</p>
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="email" placeholder="Email" />
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="password" placeholder="Password" />
+        <button className="w-24 p-1 border border-purple-800 rounded-md">Log In</button>
+        <p className="text-xs">First time in Cheve Stickies? <Link to="/sign-up"><strong>Sign Up</strong></Link> </p>
+      </form>
+    </div>
   </section>
 );
 
