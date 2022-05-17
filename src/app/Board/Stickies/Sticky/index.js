cc68849508dbf224421b8e583b0959294300c503
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 15:59:17 -0500

    Modify anchor tag to link Sign Up page

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,13 +1,15 @@
 const Sticky = () => (
-  <div>
-    <header>
-      <div>Title</div>
-      <div>•••</div>
-      <div><bold>x</bold></div>
-    </header>
-    <main>
-      Text
-    </main>
+  <div className="w-96 bg-yellow-300 border border-yellow-300 rounded-md">
+    <div className="flex justify-between p-2 border border-yellow-700 rounded-md">
+      <p className="w-auto">Title</p>
+      <div className="w-10 flex justify-between">
+      <button>•••</button>
+      <button><bold>×</bold></button>
+      </div>
+    </div>
+    <div>
+      Chevechuvi Rondon es una bebe muy hermosa que es cariñosa y preciosa.
+    </div>
   </div>
 );
 
