d7737232a517633d03e9dabfea769903935d8046
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 16:23:10 -0500

    Finish first layout prototype in Stickies component

--- src/app/Board/Stickies/index.js
+++ src/app/Board/Stickies/index.js
@@ -2,12 +2,14 @@ import Sticky from "./Sticky";
 
 const data = [
   { id:1, title: 'none', text: "Chevechuvi es lo mas" },
-  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello" }
+  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello" },
+  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" },
+  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" }
 ];
 
 const Stickies = () => {
   return (
-    <section>
+    <section className="flex flex-wrap">
       {data.map((sticky, key) => <Sticky id={key.key} title={sticky.title} text={sticky.text} />)}
     </section>
   )
