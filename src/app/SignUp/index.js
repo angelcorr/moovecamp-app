6058dee0f6b1960a44089b72b01927ea85fc0a8a
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-17 16:07:49 -0500

    Modify Sign Up component and finish first prototype

--- src/app/SignUp/index.js
+++ src/app/SignUp/index.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => (
-  <form>
-    <input placeholder="Email" />
-    <input placeholder="Confirm your email" />
-    <input placeholder="Password" />
-    <input placeholder="Confirm your Password" />
-    <button>Sign Up</button>
-  </form>
+  <section className="bg-sticky-background-login bg-cover bg-center">
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-96 h-96 flex flex-col justify-evenly items-center border border-purple-800 rounded-md">
+        <p className="text-2xl">Sign Up</p>
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="email" placeholder="Email" />
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="email" placeholder="Confirm your email" />
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="password" placeholder="Password" />
+        <input className="w-80 p-2.5 border border-purple-800 rounded-md" type="password" placeholder="Confirm your Password" />
+        <button className="w-24 p-1 border border-purple-800 rounded-md">Sign Up</button>
+        <p className="text-xs">Do you have an account? <Link to="/"><strong>Log In</strong></Link></p>
+      </form>
+    </div>
+  </section>
 );
 
 export default SignUp;
