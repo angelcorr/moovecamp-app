06701516c9ba06f38a840971bc752d98a60daa7c
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 17:42:45 -0500

    Add border to Stikies and Toolbar components

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,5 +1,5 @@
 const Sticky = ({title, text}) => (
-  <div className="w-96 m-1 bg-yellow-300 border border-yellow-700 rounded-md">
+  <div className="w-96 m-2 bg-yellow-300 border border-yellow-700 rounded-md">
     <header className="flex justify-between p-2 border border-b-2 border-yellow-700 rounded-t-md">
       <p className="w-auto">{title}</p>
       <div className="w-10 flex justify-between">
