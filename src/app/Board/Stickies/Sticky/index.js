6dacd6ee844ae5335eb3004812a1e44fdcd2f180
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 13:30:12 -0500

    Change rounded border to only rounded top border

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,6 +1,6 @@
 const Sticky = () => (
   <div className="w-96 m-1 bg-yellow-300 border border-yellow-300 rounded-md">
-    <div className="flex justify-between p-2 border border-yellow-700 rounded-md">
+    <div className="flex justify-between p-2 border border-yellow-700 rounded-t-lg">
       <p className="w-auto">Title</p>
       <div className="w-10 flex justify-between">
       <button>•••</button>
