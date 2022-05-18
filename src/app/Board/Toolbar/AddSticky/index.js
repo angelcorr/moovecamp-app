d2bf9d79e36e084102d438370a47bd85de97ca09
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 13:26:54 -0500

    Improve AddSticky component layout

--- src/app/Board/Toolbar/AddSticky/index.js
+++ src/app/Board/Toolbar/AddSticky/index.js
@@ -1,5 +1,7 @@
 const AddSticky = () => (
-  <button>+</button>
+  <div className=" flex self-center">
+    <button className="w-10 h-10 bg-gray-100 text-gray-700 border border-gray-300 rounded-full transition ease-in-out delay-150 hover:scale-125 duration-300">+</button>
+  </div>
 );
 
 export default AddSticky;
