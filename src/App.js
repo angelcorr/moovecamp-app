690c06f09bca9264e8bc1a1fe30fa94359d3c2f7
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 17:43:26 -0500

    Delete unnecessary css file from App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { Routes, Route } from "react-router-dom";
 import React from 'react';
 
-import './App.css';
 import Board from './app/Board';
 import Login from './app/Login';
 import SignUp from './app/SignUp';
