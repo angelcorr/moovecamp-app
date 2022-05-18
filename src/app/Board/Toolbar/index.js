268f0199a6b09589e21dcac3c3cbf65d7359dbca
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 09:31:43 -0500

    Finish first layout prototype in Toolbar componen

--- src/app/Board/Toolbar/index.js
+++ src/app/Board/Toolbar/index.js
@@ -2,9 +2,11 @@ import AddSticky from "./AddSticky";
 import Trash from "./Trash";
 
 const Toolbar = () => (
-  <div>
-    <Trash />
-    <AddSticky />
+  <div className="flex justify-center">
+    <div className="w-64 h-9 flex justify-around m-3 border border-black">
+      <Trash />
+      <AddSticky />
+    </div>
   </div>
 );
 
