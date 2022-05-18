ef705d4d3f87a58ed0d1b7c6758ff7ef96533e47
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 10:03:54 -0500

    Finish first prototype of Trash component and change image trash localization

--- src/app/Board/Toolbar/Trash/index.js
+++ src/app/Board/Toolbar/Trash/index.js
@@ -1,5 +1,17 @@
-const Trash = () => (
-  <img alt="Trash can" />
-);
+import emptyTrash from './trash-can/trash-can-empty.png';
+import fullTrash from './trash-can/trash-can-full.png'
+
+const Trash = () => {
+  const isEmpty = false;
+
+  return (
+    <>
+      {
+        !isEmpty ? <img src={emptyTrash} alt="Trash can empty" /> : 
+        <img src={fullTrash} alt="Trash can full" /> 
+      }
+    </>
+  )
+};
 
 export default Trash;
