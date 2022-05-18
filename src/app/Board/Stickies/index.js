8fd2f539063006159e850efd56241f553e77b887
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 15:13:47 -0500

    Improve the map function and change the data to see the sizes of all the stickies

--- src/app/Board/Stickies/index.js
+++ src/app/Board/Stickies/index.js
@@ -1,16 +1,19 @@
 import Sticky from "./Sticky";
 
 const data = [
-  { id:1, title: 'none', text: "Chevechuvi es lo mas" },
-  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello" },
-  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" },
-  { id:2, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" }
+  { id: 1, title: 'none', text: "Chevechuvi" },
+  { id: 2, title: 'yeska', text: "Chevechuvi es lo mas bello" },
+  { id: 3, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo sisisis" },
+  { id: 4, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo no se nada de eso aun que broma:(" },
+  { id: 5, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo b" },
+  { id: 6, title: 'yeska', text: "Chevechuvi es" },
+  { id: 7, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" }
 ];
 
 const Stickies = () => {
   return (
     <div className="flex flex-wrap">
-      {data.map((sticky, key) => <Sticky id={key.key} title={sticky.title} text={sticky.text} />)}
+      {data.map((sticky) => <Sticky key={sticky.id} title={sticky.title} text={sticky.text} />)}
     </div>
   )
 };
