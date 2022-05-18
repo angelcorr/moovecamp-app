12d4f8c5a3c5842aac292c7427fa8a9c620d0594
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-18 08:40:42 -0500

    Finish first prototype in Board component

--- src/app/Board/index.js
+++ src/app/Board/index.js
@@ -2,7 +2,7 @@ import Stickies from "./Stickies";
 import Toolbar from './Toolbar';
 
 const Board = () => (
-  <section>
+  <section className="h-screen m-1 flex flex-col justify-between">
     <Stickies />
     <Toolbar />
   </section>
