a180c42ae6b8e2f1724d3e79c938f1d2eaa4b52a
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-20 13:27:54 -0500

    Add contenteditable attribute functionality, removeSticky and useDispatch and useState methods to Sticky component

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,16 +1,31 @@
-const Sticky = ({title, text}) => (
-  <div className="w-96 m-2 bg-yellow-300 border border-yellow-700 rounded-md">
-    <header className="flex justify-between p-2 border border-b-2 border-yellow-700 rounded-t-md">
-      <p className="w-auto">{title}</p>
-      <div className="w-10 flex justify-between">
-      <button>•••</button>
-      <button>×</button>
+import { useDispatch } from "react-redux";
+import { removeSticky } from "../../../redux/stickiesSlice";
+
+const Sticky = ({title, text, id}) => {
+  const dispatch = useDispatch();
+  const contentText = (event) => {
+    if(event) {
+      console.log('event', event);
+      const element = document.getElementById('contentText');
+      element.setAttribute('contenteditable', 'true');
+      console.log('element', element);
+    }
+  }
+
+  return (
+    <div className="w-96 m-2 bg-yellow-300 border border-yellow-700 rounded-md" id={id}>
+      <header className="flex justify-between p-2 border border-b-2 border-yellow-700 rounded-t-md">
+        <p className="w-auto">{title}</p>
+        <div className="w-10 flex justify-between">
+        <button>•••</button>
+        <button onClick={() => dispatch(removeSticky({ id }))}>×</button>
+        </div>
+      </header>
+      <div className="p-2" id="contentText" onClick={(event) => contentText(event)}>
+        {text}
       </div>
-    </header>
-    <div className="p-2">
-      {text}
     </div>
-  </div>
-);
+  )
+};
 
 export default Sticky;
