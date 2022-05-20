1645017928438879690d9386e8c1e5f9b950d69b
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-20 13:35:16 -0500

    Change first prototype to add useDispatch and make the add button reactive

--- src/app/Board/Toolbar/AddSticky/index.js
+++ src/app/Board/Toolbar/AddSticky/index.js
@@ -1,7 +1,19 @@
-const AddSticky = () => (
-  <div className=" flex self-center">
-    <button className="w-10 h-10 bg-gray-100 text-gray-700 border border-gray-300 rounded-full transition ease-in-out delay-150 hover:scale-125 duration-300">+</button>
-  </div>
-);
+import { useDispatch } from "react-redux";
+import { addSticky } from "../../../redux/stickiesSlice";
+
+const AddSticky = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex self-center">
+      <button
+        onClick={() => dispatch(addSticky({title: "cheve", text: "grevegruvi"}))}
+        className="w-10 h-10 bg-gray-100 text-gray-700 border border-gray-300 rounded-full transition ease-in-out delay-150 hover:scale-125 duration-300"
+      >
+        +
+      </button>
+    </div>
+  )
+};
 
 export default AddSticky;
