4f47102f875422e797dc8012e945eca0e69eb5ca
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-20 13:36:49 -0500

    Change hardcode to use useSelector and get the current stickies state to map it

--- src/app/Board/Stickies/index.js
+++ src/app/Board/Stickies/index.js
@@ -1,19 +1,13 @@
 import Sticky from "./Sticky";
-
-const data = [
-  { id: 1, title: 'none', text: "Chevechuvi" },
-  { id: 2, title: 'yeska', text: "Chevechuvi es lo mas bello" },
-  { id: 3, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo sisisis" },
-  { id: 4, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo no se nada de eso aun que broma:(" },
-  { id: 5, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo b" },
-  { id: 6, title: 'yeska', text: "Chevechuvi es" },
-  { id: 7, title: 'yeska', text: "Chevechuvi es lo mas bello de nuevo" }
-];
+import { selectStickies } from '../../redux/stickiesSlice'
+import { useSelector } from "react-redux";
 
 const Stickies = () => {
+  const stickies = useSelector(selectStickies);
+
   return (
     <div className="flex flex-wrap">
-      {data.map((sticky) => <Sticky key={sticky.id} title={sticky.title} text={sticky.text} />)}
+      {stickies.map((sticky) => <Sticky key={sticky.id} id={sticky.id} title={sticky.title} text={sticky.text} />)}
     </div>
   )
 };
