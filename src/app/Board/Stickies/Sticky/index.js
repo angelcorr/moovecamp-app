5a4fd466f440300791a691266c7a5237a568b820
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-24 11:23:54 -0500

    Change onClick attribute from Sticky component to Stickies component and improve the handleContentText function

--- src/app/Board/Stickies/Sticky/index.js
+++ src/app/Board/Stickies/Sticky/index.js
@@ -1,27 +1,19 @@
 import { useDispatch } from "react-redux";
 import { removeSticky } from "../../../redux/stickiesSlice";
 
-const Sticky = ({title, text, id}) => {
+const Sticky = ({title, text, id, handleContent}) => {
   const dispatch = useDispatch();
-  const contentText = (event) => {
-    if(event) {
-      console.log('event', event);
-      const element = document.getElementById('contentText');
-      element.setAttribute('contenteditable', 'true');
-      console.log('element', element);
-    }
-  }
 
   return (
     <div className="w-96 m-2 bg-yellow-300 border border-yellow-700 rounded-md" id={id}>
       <header className="flex justify-between p-2 border border-b-2 border-yellow-700 rounded-t-md">
-        <p className="w-auto">{title}</p>
+        <p className="w-auto" id={id} onClick={handleContent}>{title}</p>
         <div className="w-10 flex justify-between">
         <button>•••</button>
         <button onClick={() => dispatch(removeSticky({ id }))}>×</button>
         </div>
       </header>
-      <div className="p-2" id="contentText" onClick={(event) => contentText(event)}>
+      <div className="p-2" id={id} onClick={handleContent}>
         {text}
       </div>
     </div>
--- src/app/Board/Stickies/index.js
+++ src/app/Board/Stickies/index.js
@@ -4,10 +4,23 @@ import { useSelector } from "react-redux";
 
 const Stickies = () => {
   const currentStickies = useSelector(selectStickies);
+  console.log('currentStickies', currentStickies);
+  const handleContentText = (event) => {
+    const elementId = event.target;
+    elementId.setAttribute("contenteditable", "true");
+  }
 
   return (
     <div className="flex flex-wrap">
-      {currentStickies.stickies.map((sticky) => <Sticky key={sticky.id} id={sticky.id} title={sticky.title} text={sticky.text} />)}
+      {currentStickies.stickies.map((sticky) =>
+        <Sticky
+          key={sticky.id}
+          id={sticky.id}
+          title={sticky.title}
+          text={sticky.text}
+          handleContent={handleContentText}
+        />
+      )}
     </div>
   )
 };
