45c5dbb0e544a413613b1eed5271381bfa409594
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-26 16:23:40 -0500

    Change initial stickies state from Stickies Slice

--- src/app/redux/stickiesSlice.js
+++ src/app/redux/stickiesSlice.js
@@ -1,24 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialStickies = [
-  { id: 1, title: 'nonel', text: 'Chevechuvi' },
-  { id: 2, title: 'yeska', text: 'Chevechuvi es lo mas bello' },
-  { id: 3, title: 'yeska', text: 'Chevechuvi es lo mas bello de nuevo sisisis' },
-  {
-    id: 4,
-    title: 'yeska',
-    text: 'Chevechuvi es lo mas bello de nuevo no se nada de eso aun que broma:(',
-  },
-  { id: 5, title: 'yeska', text: 'Chevechuvi es lo mas bello de nuevo b' },
-  { id: 6, title: 'yeska', text: 'Chevechuvi es' },
-  { id: 7, title: 'yeska', text: 'Chevechuvi es lo mas bello de nuevo' },
-];
-
 //Top level state for Stickies
 export const stickiesSlice = createSlice({
   name: 'stickies',
   initialState: {
-    stickies: initialStickies,
+    stickies: [],
     deletedStickies: [],
   },
   reducers: {
