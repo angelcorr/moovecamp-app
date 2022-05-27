63fc56bcc276319f52db3d94963fe01237a32b92
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-27 14:57:37 -0500

    Fix modal position

--- src/app/Board/Modal/index.js
+++ src/app/Board/Modal/index.js
@@ -1,7 +1,7 @@
 const Modal = ({ title, children, visible, setVisible }) => {
   return (
     visible && (
-      <div className="w-6/12 h-3/4 fixed top-24 flex justify-center items-center rounded-md bg-slate-100 border border-2 border-zinc-500 p-4">
+      <div className="w-6/12 h-3/4 fixed top-24 left-1/4 flex justify-center items-center rounded-md bg-slate-100 border border-2 border-zinc-500 p-4">
         <span className="Modal-backdrop"></span>
         <div className="w-full h-full">
           <div className="p-3 flex justify-between border border-b-zinc-400">
