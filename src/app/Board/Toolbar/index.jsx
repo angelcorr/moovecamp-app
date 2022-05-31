75addf33a8ab14eabd49efec043033626f69644a
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-31 08:04:03 -0500

    Delete Tailwind width hardcode class from Toolbar

--- src/app/Board/Toolbar/index.jsx
+++ src/app/Board/Toolbar/index.jsx
@@ -4,7 +4,7 @@ import Trash from './Trash';
 
 const Toolbar = () => (
   <div className="flex justify-center sticky inset-x-0 bottom-1 md:static">
-    <div className="w-64 h-16 flex justify-around m-3 bg-gray-100 border border-zinc-400 rounded-md shadow-2xl">
+    <div className="h-16 flex justify-around m-3 bg-gray-100 border border-zinc-400 rounded-md shadow-2xl">
       <Trash />
       <AddSticky />
     </div>
