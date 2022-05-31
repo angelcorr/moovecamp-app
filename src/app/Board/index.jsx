b30a20172f71795e6784ff6839f65e9e0186999a
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-31 10:34:37 -0500

    Add a validator to restrict if the user does not have an account

--- src/app/Board/index.jsx
+++ src/app/Board/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectUserLoggedIn } from '../redux/usersSlice';
 import Stickies from './Stickies';
 import Toolbar from './Toolbar';
 
-const Board = () => (
-  <section className="h-screen flex flex-col justify-between overflow-auto border border-zinc-500 border-16 rounded-lg bg-slate-100">
-    <Stickies />
-    <Toolbar />
-  </section>
-);
+const Board = () => {
+  const currentUser = useSelector(selectUserLoggedIn);
+
+  if (!currentUser) return <Navigate to="/" />;
+
+  return (
+    <section className="h-screen flex flex-col justify-between overflow-auto border border-zinc-500 border-16 rounded-lg bg-slate-100">
+      <Stickies />
+      <Toolbar />
+    </section>
+  );
+};
 
 export default Board;
