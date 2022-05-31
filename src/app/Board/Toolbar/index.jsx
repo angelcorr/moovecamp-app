a3c71087f84be933cf93763d3bb0e6b7b1ae85dc
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-31 10:28:50 -0500

    Add SignOut and CurrentUser components to Toolbar component

--- src/app/Board/Toolbar/index.jsx
+++ src/app/Board/Toolbar/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AddSticky from './AddSticky';
+import CurrentUser from './CurrentUser';
+import SignOut from './SignOut';
 import Trash from './Trash';
 
 const Toolbar = () => (
   <div className="flex justify-center sticky inset-x-0 bottom-1 md:static">
     <div className="h-16 flex justify-around m-3 bg-gray-100 border border-zinc-400 rounded-md shadow-2xl">
+      <CurrentUser />
+      <SignOut />
       <Trash />
       <AddSticky />
     </div>
