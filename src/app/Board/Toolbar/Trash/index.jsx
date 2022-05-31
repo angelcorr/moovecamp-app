f0f78622b5de2a9b628c37c239c35b4855104b39
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-05-31 11:18:39 -0500

    Add TrashModal to Trash component and delete unnecessary imports

--- src/app/Board/Toolbar/Trash/index.jsx
+++ src/app/Board/Toolbar/Trash/index.jsx
@@ -2,9 +2,9 @@ import { React, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeAllStickiesFromTrash, selectStickies } from '../../../redux/stickiesSlice';
 import Modal from '../../Modal';
-import Sticky from '../../Stickies/Sticky';
 import emptyTrash from './trash-can/trash-can-empty.png';
 import fullTrash from './trash-can/trash-can-full.png';
+import TrashModal from './TrashModal';
 
 const Trash = () => {
   const currentState = useSelector(selectStickies);
@@ -29,41 +29,7 @@ const Trash = () => {
         </button>
       )}
       <Modal title="Trash can" visible={modalVisible} setVisible={setModalVisible}>
-        <div className="h-4/5 md:w-full overflow-auto flex flex-col items-center">
-          {currentState.deletedStickies.length <= 0 ? (
-            <p className="w-full p-4 flex justify-center">The trash is empty</p>
-          ) : (
-            currentState.deletedStickies.map((sticky) => (
-              <Sticky
-                key={sticky.id}
-                id={sticky.id}
-                title={sticky.title}
-                text={sticky.text}
-                color={sticky.color}
-                font={sticky.font}
-                isInsideTrash
-              />
-            ))
-          )}
-        </div>
-        <footer className="sticky my-2 flex justify-evenly">
-          <button
-            type="button"
-            className="w-4/12 p-2 text-sm border border-red-600 bg-red-600 text-zinc-50 rounded-md transition ease-in-out delay-150 hover:scale-110 duration-300"
-            onClick={() => setModalVisible(false)}
-          >
-            Cancel
-          </button>
-          {currentState.deletedStickies.length > 0 && (
-            <button
-              type="button"
-              className="w-4/12 p-2 text-sm border border-red-600 bg-red-600 text-zinc-50 rounded-md transition ease-in-out delay-150 hover:scale-110 duration-300"
-              onClick={() => handleEmptyTrash()}
-            >
-              Empty Trash
-            </button>
-          )}
-        </footer>
+        <TrashModal handleTrash={handleEmptyTrash} setVisible={setModalVisible} />
       </Modal>
     </>
   );
