9bd6635b5cd7bc5447d6c3414bec82900bfd45c4
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-06-02 12:12:41 -0500

    Create Header component to display the current user by email and the sign out button

---
+++ src/app/Board/header/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserLoggedIn, signOut } from '../../redux/usersSlice';
+
+const Header = () => {
+  const { email } = useSelector(selectUserLoggedIn);
+  const dispatch = useDispatch();
+
+  return (
+    <header className="p-2 flex justify-evenly border border-b-zinc-500 border-b-2">
+      <div className="flex items-center">
+        {email}
+      </div>
+      <div>
+        <button className="p-2 border border-zinc-500 rounded-lg transition ease-in-out delay-150 hover:scale-125 duration-300" type="button" onClick={() => dispatch(signOut())}>Sign out</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
