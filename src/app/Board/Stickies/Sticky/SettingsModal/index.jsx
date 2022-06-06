976650ef4d9c917f7cc713cc27f1d80873e4a01a
Ángeles Rondón <angelesemilianaa@gmail.com>
2022-06-06 12:42:27 -0500

    Add dinamic className value to improve UI experience

--- src/app/Board/Stickies/Sticky/SettingsModal/index.jsx
+++ src/app/Board/Stickies/Sticky/SettingsModal/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeSticky }) => (
+const SettingsModal = ({
+  setBackgroundCColor, setSelectFontFamily, handleChangeSticky, selectFontFamily, backgroundColor,
+}) => (
   <div className="h-5/6 flex flex-col justify-evenly items-center">
     <p>Change sticky`s background color</p>
     <div>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-white bg-orange-600 border border-orange-600 rounded-lg"
+        className={backgroundColor === 'orange'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-orange-600 shadow-lg shadow-orange-600/50 border border-orange-600 rounded-lg scale-110'
+          : 'mt-3.5 mr-7 p-2 text-white bg-orange-600 border border-orange-600 rounded-lg'}
         value="orange"
         onClick={(event) => setBackgroundCColor(event.target.value)}
       >
@@ -15,7 +19,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
       </button>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-white bg-blue-700 border border-blue-700 rounded-lg"
+        className={backgroundColor === 'blue'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-blue-700 shadow-lg shadow-blue-700/50 border border-blue-700 rounded-lg scale-110'
+          : 'mt-3.5 mr-7 p-2 text-white bg-blue-700 border border-blue-700 rounded-lg'}
         value="blue"
         onClick={(event) => setBackgroundCColor(event.target.value)}
       >
@@ -23,7 +29,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
       </button>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-white bg-purple-700 border border-purple-700 rounded-lg"
+        className={backgroundColor === 'purple'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-purple-700 shadow-lg shadow-purple-700/50 border border-purple-700 rounded-lg scale-110'
+          : 'mt-3.5 mr-7 p-2 text-white bg-purple-700 border border-purple-700 rounded-lg'}
         value="purple"
         onClick={(event) => setBackgroundCColor(event.target.value)}
       >
@@ -31,7 +39,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
       </button>
       <button
         type="button"
-        className="mt-3.5 p-2 text-white bg-green-700 border border-green-700 rounded-lg"
+        className={backgroundColor === 'green'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-green-700 shadow-lg shadow-green-700/50 border border-green-700 rounded-lg scale-110'
+          : 'mt-3.5 p-2 text-white bg-green-700 border border-green-700 rounded-lg'}
         value="green"
         onClick={(event) => setBackgroundCColor(event.target.value)}
       >
@@ -42,7 +52,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
     <div>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-sans"
+        className={selectFontFamily === 'sansSerif'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-black border border-black rounded-lg font-sans scale-110'
+          : 'mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-sans'}
         value="sansSerif"
         onClick={(event) => setSelectFontFamily(event.target.value)}
       >
@@ -50,7 +62,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
       </button>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-serif"
+        className={selectFontFamily === 'serif'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-black border border-black rounded-lg font-serif scale-110'
+          : 'mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-serif'}
         value="serif"
         onClick={(event) => setSelectFontFamily(event.target.value)}
       >
@@ -58,7 +72,9 @@ const SettingsModal = ({ setBackgroundCColor, setSelectFontFamily, handleChangeS
       </button>
       <button
         type="button"
-        className="mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-mono"
+        className={selectFontFamily === 'monospace'
+          ? 'mt-3.5 mr-7 p-2 text-white bg-black border border-black rounded-lg font-mono scale-110'
+          : 'mt-3.5 mr-7 p-2 text-black bg-zinc-300 border border-zinc-500 rounded-lg font-mono'}
         value="monospace"
         onClick={(event) => setSelectFontFamily(event.target.value)}
       >
@@ -79,6 +95,8 @@ SettingsModal.propTypes = {
   setSelectFontFamily: PropTypes.func.isRequired,
   setBackgroundCColor: PropTypes.func.isRequired,
   handleChangeSticky: PropTypes.func.isRequired,
+  selectFontFamily: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
 };
 
 export default SettingsModal;
